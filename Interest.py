07cdcd309e1bcd754bf25a6c8be0b7cdeb154a89
wolf-bailang <zjb5633397@sina.com>
2021-01-30 02:02:24 +0800

    add Interest.py

---
+++ Interest.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+'''
+interest = {'route_ID': [interest_ID, consumer_ID, route_ID, content_name, start_time, life_time]}
+'''
+
+def Drop_interest(inface, interest):
+    print('2')
+
+
+if __name__ == '__main__':
+    print('interest')
+
