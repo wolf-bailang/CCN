9514d3bad9042dea5a0a413502076789d3f26d7a
shpeng0607 <47897036+shpeng0607@users.noreply.github.com>
2021-02-22 16:34:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 def main():
     server_num = 4
     PS_init(route_num=4, content_num=100)
+    start_time = time.time()
     # data = unique_strings(k=4, ntokens=400)
     # data = list(data)
     # data = [data[:100], data[101:200], data[201:300], data[301:400]]
@@ -17,15 +18,9 @@ def main():
         server = Server(i)
         server.start()
         server_list.append(server)
-
-    for id , i in enumerate(server_list):
-        i.start_network()
-#         interest_data = {"data": data[2][2], "id": 8000 + id, "time": time.time()}
-#         i.send_interest(interest_data)
-#         interest_data = {"data": data[1][1], "id": 8000 + id, "time": time.time()}
-#         i.send_interest(interest_data)
-#         interest_data = {"data": data[3][0], "id": 8000 + id, "time": time.time()}
-#         i.send_interest(interest_data)
+        
+    for i in server_list:
+        i.start_network(start_time)
         
     while True:
         if time.time() - start_time > 10:
